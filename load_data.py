e706433a55c5b488cc636812106c68aacebde076
ereindersss <e.f.h.c.m.reinders@student.tudelft.nl>
2026-02-24 14:24:21 +0100

    added a line to load data

--- load_data.py
+++ load_data.py
@@ -1,3 +1,4 @@
+#%% load data 
 import pandas as pd
 import os
 import zipfile
@@ -9,3 +10,5 @@ def load_data():
     this_directory = os.path.dirname(os.path.abspath(__file__))
     data = pd.read_csv(os.path.join(this_directory, 'ecg_data/ecg_data.csv'), index_col=0)
     return data
+
+load_data()
