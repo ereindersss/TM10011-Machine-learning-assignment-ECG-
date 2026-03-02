7b18f98b02c1e0311b5c62a246572a3bb7069234
ereindersss <e.f.h.c.m.reinders@student.tudelft.nl>
2026-03-02 22:14:26 +0100

    completed merge

--- assignment.py
+++ assignment.py
@@ -5,10 +5,10 @@ import zipfile
 
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns 
 
 from sklearn import datasets as ds
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
 from sklearn.decomposition import PCA
 from sklearn import metrics
 from sklearn import model_selection
@@ -24,6 +24,19 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import neighbors
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, RobustScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+import sklearn.metrics as sklm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.neural_network import MLPClassifier
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.tree import plot_tree
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_curve, auc, confusion_matrix, classification_report
+
 #%% load data 
 with zipfile.ZipFile("ecg_data.zip","r") as zip_ref:
     zip_ref.extractall("ecg_data")
@@ -33,7 +46,26 @@ def load_data():
     data = pd.read_csv(os.path.join(this_directory, 'ecg_data/ecg_data.csv'), index_col=0)
     return data
 
-data = load_data()
+#%% examine the data
+raw_data = load_data()
+
+print(f'The number of samples: {len(raw_data.index)}')
+print(f'The number of columns: {len(raw_data.columns)}')
+
+#print(raw_data.head())
+print(f'The number of NaN values in the entire dataframe: {raw_data.isnull().sum().sum()}')
+print(f'The number of samples with label 0: {len(raw_data[raw_data["label"] == 0])}')
+print(f'The number of samples with label 1: {len(raw_data[raw_data["label"] == 1])}')
+print(f'The percentage of samples with label 0 is thus {len(raw_data[raw_data["label"] == 0])/len(raw_data.index)*100:.2f}%', 
+      f'and the percentage with label 1 {len(raw_data[raw_data["label"] == 1])/len(raw_data.index)*100:.2f}%')
+
+# print(raw_data.groupby('label').count())
+# print(raw_data.groupby('label').mean())
+# print(raw_data.groupby('label').var())
+# print(raw_data.groupby('label').std())
+
+data = raw_data
+
 
 #%% defining useful functions 
 def colorplot(clf, ax, x, y, h=100, precomputer=None):
@@ -437,7 +469,7 @@ for validation_index, test_index in cv_20fold.split(X, Y):
 
 # Create results dataframe and plot it
 results = pd.DataFrame(results)
-seaborn.boxplot(y='auc', x='set', data=results)
+sns.boxplot(y='auc', x='set', data=results)
 
 optimal_n = int(np.median(best_n_neighbors))
 
@@ -464,64 +496,12 @@ score_test = knn.score(X_test_pca, y_test)
 
 # Print result
 print(f"Training result: {score_train}")
-print(f"Test result: {score_test}")#%% import packages 
-import pandas as pd
-import os
-import zipfile
-from matplotlib import pyplot as plt
-import numpy as np
-import seaborn as sns
+print(f"Test result: {score_test}")
 
-from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, RobustScaler
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-import sklearn.metrics as sklm
-from sklearn.ensemble import RandomForestClassifier
-#from MLstatkit.stats import Delong_test
-from sklearn.impute import SimpleImputer
-from sklearn.neural_network import MLPClassifier
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.tree import plot_tree
-#from mlxtend.evaluate import mcnemar_table, mcnemar
-from sklearn.metrics import precision_score, recall_score, f1_score, roc_curve, auc, confusion_matrix, classification_report
 
-#%% load data 
-with zipfile.ZipFile("ecg_data.zip","r") as zip_ref:
-    zip_ref.extractall("ecg_data")
 
-def load_data():
-    this_directory = os.path.dirname(os.path.abspath(__file__))
-    data = pd.read_csv(os.path.join(this_directory, 'ecg_data/ecg_data.csv'), index_col=0)
-    return data
 
-#%% examine the data
-raw_data = load_data()
 
-print(f'The number of samples: {len(raw_data.index)}')
-print(f'The number of columns: {len(raw_data.columns)}')
-
-#print(raw_data.head())
-print(f'The number of NaN values in the entire dataframe: {raw_data.isnull().sum().sum()}')
-print(f'The number of samples with label 0: {len(raw_data[raw_data["label"] == 0])}')
-print(f'The number of samples with label 1: {len(raw_data[raw_data["label"] == 1])}')
-print(f'The percentage of samples with label 0 is thus {len(raw_data[raw_data["label"] == 0])/len(raw_data.index)*100:.2f}%', 
-      f'and the percentage with label 1 {len(raw_data[raw_data["label"] == 1])/len(raw_data.index)*100:.2f}%')
-
-# print(raw_data.groupby('label').count())
-# print(raw_data.groupby('label').mean())
-# print(raw_data.groupby('label').var())
-# print(raw_data.groupby('label').std())
-
-data = raw_data
-
-#%% split data in train and test set
-X = data.drop('label', axis=1)
-Y = data['label']
-
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=5, stratify=Y)
 
 #%% principal component analysis (PCA)
 categorical_cols = x_train.select_dtypes(include=['object', 'category']).columns
